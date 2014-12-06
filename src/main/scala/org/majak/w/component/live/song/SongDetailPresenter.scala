9ccd403cf08b1fb28e0d58c44e668250c14e40a4
Martin Krajc <krajc.martin@gmail.com>
2014-12-06 17:27:48 +0100

    song detail started

---
+++ src/main/scala/org/majak/w/component/live/song/SongDetailPresenter.scala
@@ -0,0 +1,13 @@
+package org.majak.w.component.live.song
+
+import org.majak.w.model.SongModel.Song
+import org.majak.w.ui.mvp.Presenter
+
+/**
+ * Represents whole song on screen
+ */
+class SongDetailPresenter(var song: Song) extends Presenter[SongDetailView]{
+
+
+
+}
---
+++ src/main/scala/org/majak/w/component/live/song/SongDetailView.scala
@@ -0,0 +1,10 @@
+package org.majak.w.component.live.song
+
+import org.majak.w.ui.mvp.View
+
+/**
+ * Represent UI for song detail component
+ */
+trait SongDetailView extends View {
+
+}
