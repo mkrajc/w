7fce12cda02056dc2ebb093552a582badb0f7750
martin.krajc <krajc.martin@gmail.com>
2015-01-19 16:50:19 +0100

    refresh song detail on refresh

--- src/main/scala/org/majak/w/component/live/song/SongDetailPresenter.scala
+++ src/main/scala/org/majak/w/component/live/song/SongDetailPresenter.scala
@@ -2,31 +2,44 @@ package org.majak.w.component.live.song
 
 import org.majak.w.model.song.controller.SongController
 import org.majak.w.model.song.data.SongModel.Song
-import org.majak.w.model.song.rx.{SongEvent, SongSelected}
+import org.majak.w.model.song.rx.{SongsRefreshed, SongEvent, SongSelected}
 import org.majak.w.ui.mvp.Presenter
 
 /**
  * Represents whole song on screen
  */
 class SongDetailPresenter(songController: SongController) extends Presenter[SongDetailView] {
-
-  def song(s: Song) = {
-    view.showSongName(s.name)
-    view.showSongParts(s.parts)
-  }
+  private var song: Option[Song] = None
 
   override protected def onBind(v: SongDetailView): Unit = {
     songController.observable.subscribe(onNext = e => handleSongEvent(e))
   }
 
-  private def handleSongEvent(event: SongEvent) = {
-    event match {
-      case SongSelected(s) => showSong(s)
-      case _ => ()
+  private def refreshSongView(songs: List[Song]) = {
+    if (song.isDefined) {
+      val found = songs.find(_.id == song.get.id)
+      if (found.isDefined) {
+        setSong(found.get)
+      } else {
+        clear()
+      }
     }
   }
 
-  private def showSong(song: Song): Unit = {
+  private def handleSongEvent(event: SongEvent) = {
+    event match {
+      case SongSelected(s) => setSong(s)
+      case SongsRefreshed(songs) => refreshSongView(songs)
+    }
+  }
+
+  private def clear() = {
+    view.clearSong()
+    this.song = None
+  }
+
+  private def setSong(song: Song): Unit = {
+    this.song = Some(song)
     view.clearSong()
     view.showSongName(song.name)
     view.showSongParts(song.parts)
